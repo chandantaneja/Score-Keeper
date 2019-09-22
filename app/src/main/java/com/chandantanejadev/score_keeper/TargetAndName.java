2bd5b231bf34af4e0e3437f1d2724b3591b57502
Chandan Taneja <chandan.taneja.931@gmail.com>
2019-09-22 19:34:42 +0530

    Solved bugs with Spinner and Visiblity

--- app/src/main/java/com/chandantanejadev/score_keeper/TargetAndName.java
+++ app/src/main/java/com/chandantanejadev/score_keeper/TargetAndName.java
@@ -14,18 +14,19 @@ import android.widget.Toast;
 
 public class TargetAndName extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    Spinner spinnerTarget = findViewById(R.id.spinner_target);
+    private Spinner spinnerTarget;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_target_and_name);
+        spinnerTarget = findViewById(R.id.spinner_target);
         EditText edNameOfTeam3 = findViewById(R.id.ed_nameOfTeam3);
         EditText edNameOfTeam4 = findViewById(R.id.ed_nameOfTeam4);
         TextView tvNameOfTeam3 = findViewById(R.id.txt_nameOfTeam3);
         TextView tvNameOfTeam4 = findViewById(R.id.txt_nameOfTeam4);
 
         if (MainActivity.PLAYER_COUNT == 2){
-            edNameOfTeam3.setVisibility(View.GONE);
+             edNameOfTeam3.setVisibility(View.GONE);
             edNameOfTeam4.setVisibility(View.GONE);
             tvNameOfTeam3.setVisibility(View.GONE);
             tvNameOfTeam4.setVisibility(View.GONE);
@@ -41,27 +42,34 @@ public class TargetAndName extends AppCompatActivity implements AdapterView.OnIt
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String selected =  parent.getItemAtPosition(position).toString();
-        Toast.makeText(this, selected, Toast.LENGTH_SHORT).show();
+        if (selected.equals("Target")){
+            //do nothing
+        }else{
+            Toast.makeText(this, selected, Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-        Toast.makeText(this, "hello bro", Toast.LENGTH_SHORT).show();
+
     }
-//    public void onRadioButtonClicked(View view) {
-//        // Is the button now checked?
-//        boolean checked = ((RadioButton) view).isChecked();
-//
-//        // Check which radio button was clicked
-//        switch(view.getId()) {
-//            case R.id.radio_targetYes:
-//                if (checked)
-//                    spinnerTarget.setVisibility(View.VISIBLE);
-//                    break;
-//            case R.id.radio_targetNo:
-//                if (checked)
-//                    spinnerTarget.setVisibility(View.GONE);
-//                    break;
-//        }
-//    }
+
+
+
+    public void onRadioButtonClicked(View view) {
+//         Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_targetYes:
+                if (checked)
+                    spinnerTarget.setVisibility(View.VISIBLE);
+                    break;
+            case R.id.radio_targetNo:
+                if (checked)
+                    spinnerTarget.setVisibility(View.GONE);
+                    break;
+        }
+    }
 }
